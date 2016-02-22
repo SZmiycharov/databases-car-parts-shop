398bb2d143a1421203de3f9a4e8f0d3c82d3c1a9
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-02-22 20:50:14 +0200

    Add new car bmw to database

--- DatabasesCarPartsShop/Program.cs
+++ DatabasesCarPartsShop/Program.cs
@@ -10,6 +10,15 @@ namespace DatabasesCarPartsShop
     {
         static void Main(string[] args)
         {
+            Car bmw = new Car()
+                {
+                    Brand = "BMW",
+                    Model = "3 series",
+                    ProductionYear = "2015"
+                };
+            CarPartsShopEntities db = new CarPartsShopEntities();
+            db.Cars.Add(bmw);
+            db.SaveChanges();
 
         }
     }
